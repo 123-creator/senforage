95bb24382ebf9c1af449e25f45d7140fb71b47b6
123-creator <diopibra96@gmail.com>
2020-01-14 14:09:42 +0000

    Correct error on heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \Illuminate\Support\Facades\Schema::defaultStringLength(191);
+
     }
     
 
